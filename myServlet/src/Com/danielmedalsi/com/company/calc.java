ed64a34fe2d261634e0a0cb1912767f2641ddf99
danielmedalsi1234 <danielmedalsi1234@gmail.com>
2018-03-10 02:46:01 +0200

    serverChatJava

---
+++ myServlet/src/Com/danielmedalsi/com/company/calc.java
@@ -0,0 +1,9 @@
+package Com.danielmedalsi.com.company;
+
+public class calc {
+    public static int calc (int firstNumber,int secondNumber){
+
+
+        return firstNumber + secondNumber;
+    }
+}
