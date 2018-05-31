ac3b9f1bc03b3d86a7d0faed5c4abb312b16517e
danielmedalsi1234 <danielmedalsi1234@gmail.com>
2018-05-31 06:31:43 +0300

    update 06:30

---
+++ ServletTzlilMakeUp/src/ChatServlet.java
@@ -0,0 +1,49 @@
+import jdk.internal.org.objectweb.asm.Handle;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@javax.servlet.annotation.WebServlet(name = "ChatServlet", urlPatterns = "/ChatServlet", description = "chat servlet")
+public class ChatServlet extends javax.servlet.http.HttpServlet {
+
+    public static final String SET_MESSAGE = "set_message=";
+    public static final String GET_MESSAGES = "get_messages=";
+    private List<String> messages;
+
+    @Override
+    public void init() throws ServletException {
+        messages = new ArrayList<>();
+
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        String queryString = request.getQueryString();
+        if(queryString == null)
+            return;
+
+        if(queryString.startsWith(GET_MESSAGES)){
+            int from = Integer.valueOf(queryString.substring(GET_MESSAGES.length()));
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = from; i < messages.size()-1; i++) {
+                stringBuilder.append(messages.get(i) + "&");
+            }
+            if(messages.size() > 0 && from <= messages.size()-1)
+                stringBuilder.append(messages.get(messages.size()-1));
+            response.getWriter().write(stringBuilder.toString());
+        }else if(queryString.startsWith(SET_MESSAGE)){
+            if(messages.size() > 300) {
+                return;
+            }
+            this.messages.add(queryString.substring(SET_MESSAGE.length()));
+            response.getWriter().write("Successfully!!");
+
+
+        }
+    }
+}
---
+++ queryStringUserNameAndPassword/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: danielmedalsi1234
+  Date: 10/03/18
+  Time: 03:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
