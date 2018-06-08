f9ca6fb28f19b6e87a10b6a9797ad5658d6270ca
danielmedalsi1234 <danielmedalsi1234@gmail.com>
2018-06-08 09:08:12 +0300

    update 2

--- FirstGoogleP/src/ServletGoogleTomcat.java
+++ FirstGoogleP/src/ServletGoogleTomcat.java
@@ -1,14 +1,13 @@
-import jdk.internal.org.objectweb.asm.Handle;
-
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-@javax.servlet.annotation.WebServlet(name = "ChatServlet", urlPatterns = "/ChatServlet", description = "chat servlet")
-public class ChatServlet extends javax.servlet.http.HttpServlet {
 
+public class ServletGoogleTomcat extends HttpServlet {
     public static final String SET_MESSAGE = "set_message=";
     public static final String GET_MESSAGES = "get_messages=";
     private List<String> messages;
@@ -16,23 +15,15 @@ public class ChatServlet extends javax.servlet.http.HttpServlet {
     @Override
     public void init() throws ServletException {
         messages = new ArrayList<>();
+        messages.add("test1");
+        messages.add("test2");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
-    }
-
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String queryString = request.getQueryString();
         if(queryString == null)
             return;
@@ -51,9 +42,7 @@ public class ChatServlet extends javax.servlet.http.HttpServlet {
                 return;
             }
             this.messages.add(queryString.substring(SET_MESSAGE.length()));
-            response.getWriter().write("Successfully!!");
-
-
+            response.getWriter().write("ok");
         }
     }
 }
